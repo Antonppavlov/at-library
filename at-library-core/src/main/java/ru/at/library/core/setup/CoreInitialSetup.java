983f842e0afa2a0cd3fbe67d3f8501b48828c915
antonpavlov <anton.it.pavlov@gmail.com>
2026-01-23 18:54:25 +0300

    доработка CoreInitialSetup.java

--- at-library-core/src/main/java/ru/at/library/core/setup/CoreInitialSetup.java
+++ at-library-core/src/main/java/ru/at/library/core/setup/CoreInitialSetup.java
@@ -22,10 +22,12 @@ import ru.at.library.core.cucumber.api.CoreEnvironment;
 import ru.at.library.core.cucumber.api.CoreScenario;
 import ru.at.library.core.utils.helpers.AssertionHelper;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Начальная настройка ядра: создание CoreEnvironment и AssertionHelper,
  * а также установка базового URL для тестов (Configuration.baseUrl).
- *
+ * <p>
  * Веб-драйвер и RestAssured настраиваются в специализированных модулях
  * (at-library-web, at-library-api) через отдельные хуки.
  */
@@ -33,23 +35,35 @@ public class CoreInitialSetup {
 
     private static final Logger log = LogManager.getLogger(CoreInitialSetup.class);
 
-    public volatile static int scenarioNumber = 1;
+    /**
+     * Счётчик запущенных сценариев в рамках одного запуска.
+     * <p>Используется только для логирования ("текущий номер / общее количество").</p>
+     */
+    public static final AtomicInteger scenarioNumber = new AtomicInteger(1);
+
+    /**
+     * Общее количество сценариев, которые будут выполнены в данном запуске.
+     * <p>Заполняется в тестовом раннере (RunFeaturesTest) при формировании {@code DataProvider}.</p>
+     */
+    public static volatile int totalScenarios = 0;
 
     @Delegate
     CoreScenario coreScenario = CoreScenario.getInstance();
 
-/**
+    /**
      * Действия выполняемые перед каждым сценарием:
      * - логирование старта сценария;
      * - создание CoreEnvironment и AssertionHelper.
-     *
+     * <p>
      * Установка baseUrl для Selenide выполняется в веб-модуле.
      */
     @Before(order = 1)
     @Step("Инициализация CoreEnvironment")
     public void initializingCoreEnvironment(Scenario scenario) throws Exception {
-        int testNumber = scenarioNumber++;
-        log.info(String.format("%s: Старт сценария %d с именем [%s]", getScenarioId(scenario), testNumber, scenario.getName()));
+        int testNumber = scenarioNumber.getAndIncrement();
+        int total = totalScenarios;
+
+        log.info(String.format("%s: Старт сценария %d/%d с именем [%s]", getScenarioId(scenario), testNumber, total, scenario.getName()));
 
         coreScenario.setEnvironment(new CoreEnvironment(scenario));
         coreScenario.setAssertionHelper(new AssertionHelper());
@@ -63,13 +77,21 @@ public class CoreInitialSetup {
     @After(order = 1)
     @Step("Завершение сценария")
     public void afterScenario(Scenario scenario) {
-        log.info(String.format("%s: Завершение сценария с именем [%s]", getScenarioId(scenario), scenario.getName()));
+        int finishedNumber = scenarioNumber.get() - 1;
+        int total = totalScenarios;
+        int remaining = total > 0 ? Math.max(total - finishedNumber, 0) : 0;
+
+        log.info(String.format(
+                "%s: Завершение сценария %d/%d с именем [%s]. Осталось сценариев: %d",
+                getScenarioId(scenario), finishedNumber, total, scenario.getName(), remaining
+        ));
+
     }
 
     /**
      * Возвращает сокращенный ID сценария
      *
-     * @return      ID сценария в формате feature_file.feature:ID
+     * @return ID сценария в формате feature_file.feature:ID
      */
     public static String getScenarioId(Scenario scenario) {
         String fullID = scenario.getId();
--- at-library-web/src/test/java/ru/at/library/web/RunFeaturesTest.java
+++ at-library-web/src/test/java/ru/at/library/web/RunFeaturesTest.java
@@ -2,6 +2,8 @@ package ru.at.library.web;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+import ru.at.library.core.setup.CoreInitialSetup;
 
 @CucumberOptions(
         monochrome = true,
@@ -11,4 +13,12 @@ import io.cucumber.testng.CucumberOptions;
         glue = {"ru"}
 )
 public class RunFeaturesTest extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        Object[][] scenarios = super.scenarios();
+        CoreInitialSetup.totalScenarios = scenarios.length;
+        return scenarios;
+    }
 }
